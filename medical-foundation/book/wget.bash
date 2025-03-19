7de8a8dadb59474f021d39df7fb57a04e691b240
Mikers <snissn@gmail.com>
2025-03-19 13:26:15 -1000

    new url

--- medical-foundation/book/wget.bash
+++ medical-foundation/book/wget.bash
@@ -1,3 +1,3 @@
 rm -rf snissn.github.io
 
-wget --mirror --convert-links --adjust-extension --page-requisites --no-parent https://snissn.github.io/back-goes-backwards/medical-foundation/combined.html
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent https://snissn.github.io/back-goes-backwards/medical-foundation/combined/combined.html
