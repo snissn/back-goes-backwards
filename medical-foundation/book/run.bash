37cc4d0cca2e71c4bb01336726ffafc55b286f86
Mikers <snissn@gmail.com>
2026-01-31 10:19:06 -0700

    update new build

--- medical-foundation/book/run.bash
+++ medical-foundation/book/run.bash
@@ -2,10 +2,22 @@
 
 #wget --mirror --convert-links --adjust-extension --page-requisites --no-parent https://snissn.github.io/back-goes-backwards/medical-foundation/combined.html
 
-pagedjs-cli ./snissn.github.io/back-goes-backwards/medical-foundation/combined/combined.html \
-  -o pagejs.pdf \
+#pagedjs-cli ./snissn.github.io/back-goes-backwards/medical-foundation/combined/combined.html \
+#  -o pagejs.pdf \
+#  --outline-tags "h1,h2,h3,h4" \
+#  --additional-script ./toc.js \
+#  --style layout.css --style ./toc.css --style page-override.css
+
+
+#new
+
+pagedjs-cli snissn.github.io/back-goes-backwards/medical-foundation/newgen/outlines/combined/combined.html \
+  -o 'body.pdf' \
   --outline-tags "h1,h2,h3,h4" \
   --additional-script ./toc.js \
   --style layout.css --style ./toc.css --style page-override.css
 
 
+
+
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile='./The Back Goes Backwards: Integrating Biomechanics into Modern Medicine By Michael Seiler and Dr. Katherine E. Gallardo, MD.pdf' lo-res-cover.pdf body.pdf
